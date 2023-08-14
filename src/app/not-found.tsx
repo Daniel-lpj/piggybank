66afe059c3e498737b0edf71f26c1059ea84d2b5
djunior <dlpjunior@apoioprodesp.sp.gov.br>
2023-08-14 20:36:00 -0300

    Primeiro commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1>404 - Página não encontrada</h1>
+      <a href="/">Voltar para página inicial</a>
+    </>
+  );
+};
+
+export default NotFound;
